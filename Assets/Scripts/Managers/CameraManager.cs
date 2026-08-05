a0623a4a34ba325ce3f098b26f2d1698a117660a
Yuukumo <1502878308@qq.com>
2026-08-05 20:06:47 +0800

    初步完成摄像头跟随玩家的逻辑并在场景设置房间进行测试

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -10,6 +10,7 @@ public class CameraManager : MonoBehaviour
     [SerializeField] List<RoomTransformer> rooms = new List<RoomTransformer>();
     [SerializeField] private RoomTransformer currentRoom;
     private bool canFollowPlayer;
+    private GameObject player;
 
     void Awake()
     {
@@ -33,6 +34,18 @@ public class CameraManager : MonoBehaviour
         if (mainCamera == null) Debug.LogError("场景中不存在 Camera，请检查场景中是否有 Camera 组件");
 
         if (currentRoom == null) Debug.LogError("CameraManager 中未设置当前房间，请检查 CameraManager 组件设置");
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player == null) Debug.LogError("场景中不存在 Player，请检查场景中是否有 Player 组件");
+    }
+
+    void LateUpdate()
+    {
+        if (canFollowPlayer && player != null)
+        {
+            Vector3 targetPosition = new Vector3(player.transform.position.x, player.transform.position.y, mainCamera.transform.position.z);
+            mainCamera.transform.position = targetPosition;
+        }
     }
 
     public void EnterRoom(RoomTransformer room)
