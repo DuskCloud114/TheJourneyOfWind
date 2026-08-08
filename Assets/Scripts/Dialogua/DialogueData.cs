85e7d9bb9b665e7e8f08bbce33beffb4a1d224f1
Yuukumo <1502878308@qq.com>
2026-08-08 20:17:57 +0800

    创建了 DIalogue 的 ScriptableObject 脚本用于配置对话文件 在场景创建了测试 UI 导入了中文字体和授权

---
+++ Assets/Scripts/Dialogua/DialogueData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewDialogueData", menuName = "Dialogue Data")]
+public class DialogueData : ScriptableObject
+{
+    [TextArea(3, 10)]
+    public string[] dialogueLines;
+}
