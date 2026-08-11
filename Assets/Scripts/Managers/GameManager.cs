a389e48a1e9d2f384d3751f3d3f8c585b1a21e16
Yuukumo <1502878308@qq.com>
2026-08-11 18:03:08 +0800

    添加了 GameManger 脚本和预制体统一管理 Manager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    public GameObject playerPrefab;
+    public GameObject UIManager;
+    public GameObject audioManager;
+    public GameObject sceneManager;
+    public GameObject skillsManager;
+    public GameObject processManager;
+    public GameObject cameraManager;
+    public GameObject windManager;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        if (playerPrefab == null) Debug.LogError("GameManager上未挂载 playerPrefab，请检查 GameManager 预制体设置");
+        if (UIManager == null) Debug.LogError("GameManager上未挂载 UIManager，请检查 GameManager 预制体设置");
+        // if (audioManager == null) Debug.LogError("GameManager上未挂载 audioManager，请检查 GameManager 预制体设置");
+        // if (sceneManager == null) Debug.LogError("GameManager上未挂载 sceneManager，请检查 GameManager 预制体设置");
+        if (skillsManager == null) Debug.LogError("GameManager上未挂载 skillsManager，请检查 GameManager 预制体设置");
+        if (processManager == null) Debug.LogError("GameManager上未挂载 processManager，请检查 GameManager 预制体设置");
+        if (cameraManager == null) Debug.LogError("GameManager上未挂载 cameraManager，请检查 GameManager 预制体设置");
+        if (windManager == null) Debug.LogError("GameManager上未挂载 windManager，请检查 GameManager 预制体设置");
+    }
+}
